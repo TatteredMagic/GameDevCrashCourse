1d6aaad3487d1d59d1006e6a3f11ad7c7a4fde7a
Tabby <56meowmix@gmail.com>
2018-02-01 15:46:14 -0700

    Part 2

--- AttackOfCuteness/main.lua
+++ AttackOfCuteness/main.lua
@@ -2,7 +2,7 @@
 -- http://MasteringCoronaSDK.com
 -- Version: 1.0
 -- Copyright 2013 J. A. Whye. All Rights Reserved.
--- "Space Cute" art by Daniel Cook (Lostgarden.com) 
+-- Images stolen from the internet
 
 -- housekeeping stuff
 
@@ -18,24 +18,56 @@ local centerY = display.contentCenterY
 
 
 -- create play screen
-local background = display.newImage("background.png")
-background.x = centerX
-background.y = centerY
-
-local planet = display.newImage("planet.png")
-planet.x = centerX
-planet.y = centerY
+local function createPlayScreen()
+	
+	--Add background to screen
+	local background = display.newImage("background.png")
+	--background.x = centerX
+	--background.y = centerY
+	
+	-- Background tranzparincy
+	background.alpha = .1
+	
+	-- Add planet to screen
+	local planet = display.newImage("planet.png")
+	planet.x = centerX
+	planet.y = display.contentHeight + 60
+	
+	-- Planet tranzparincy and size
+	planet.alpha = 0
+	planet.xScale = .1
+	planet.yScale = .1
+	
+	local function showTitle()
+		local gameTitle = display.newImage("gametitle.png")
+		gameTitle.alpha = 0
+		gameTitle.x = centerX 
+		gameTitle.y = centerY - 40
+		gameTitle.xScale = .3
+		gameTitle.yScale = .3
+		transition.to (gameTitle, {time=500, alpha=1, xScale=1, yScale=1, } )
+		
+	end
+	
+	transition.to (background, {time=2500, y=centerY, x=centerX, alpha=1} )
+	transition.to (planet, {time=2500, y=centerY, alpha=1, xScale=1, yScale=1, onComplete=showTitle} )
+	
+end	
+	
 
 -- game functions
 
 local function spawnEnemy()
-	
+	local enemy = display.newImage("beetleship.png")
+	enemy.x = math.random(20, display.contentWidth-20)
+	enemy.y = math.random(20, display.contentHeight-20)
+	enemy:addEventListener ("tap", shipSmash)
 end
 
 
 
-local function startGame()
-	
+function startGame()
+
 end
 
 
@@ -52,10 +84,13 @@ end
 
 
 
-local function shipSmash(event)
-	
+function shipSmash(event)
+	local obj = event.target
+	display.remove(obj)
+	return true
 end
 
-
+createPlayScreen()
 startGame()
 
+spawnEnemy()
