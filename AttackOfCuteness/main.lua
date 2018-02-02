a64402e3d65319c8685ac3060f5a5ea99dc11724
Tabby <56meowmix@gmail.com>
2018-02-02 14:48:58 -0700

    end of week push  section 5 14

--- AttackOfCuteness/main.lua
+++ AttackOfCuteness/main.lua
@@ -11,49 +11,11 @@ display.setStatusBar(display.HiddenStatusBar)
 local centerX = display.contentCenterX
 local centerY = display.contentCenterY
 
--- set up forward references
-
-
 -- preload audio
-
-
--- create play screen
-local function createPlayScreen()
-	
-	--Add background to screen
-	local background = display.newImage("background.png")
-	--background.x = centerX
-	--background.y = centerY
-	
-	-- Background tranzparincy
-	background.alpha = .1
-	
-	-- Add planet to screen
-	local planet = display.newImage("planet.png")
-	planet.x = centerX
-	planet.y = display.contentHeight + 60
-	
-	-- Planet tranzparincy and size
-	planet.alpha = 0
-	planet.xScale = .1
-	planet.yScale = .1
-	
-	local function showTitle()
-		local gameTitle = display.newImage("gametitle.png")
-		gameTitle.alpha = 0
-		gameTitle.x = centerX 
-		gameTitle.y = centerY - 40
-		gameTitle.xScale = .3
-		gameTitle.yScale = .3
-		transition.to (gameTitle, {time=500, alpha=1, xScale=1, yScale=1, } )
-		
-	end
-	
-	transition.to (background, {time=2500, y=centerY, x=centerX, alpha=1} )
-	transition.to (planet, {time=2500, y=centerY, alpha=1, xScale=1, yScale=1, onComplete=showTitle} )
-	
-end	
-	
+--loadstream for background sounds
+local sndKill = audio.loadSound("boing-1.wav")
+local sndBlast = audio.loadSound("blast.mp3")
+local sndLose = audio.loadSound("wahwahwah.mp3")
 
 -- game functions
 
@@ -87,10 +49,51 @@ end
 function shipSmash(event)
 	local obj = event.target
 	display.remove(obj)
+	audio.play(sndKill)
 	return true
 end
+
+
+-- create play screen
+local function createPlayScreen()
+	
+	--Add background to screen
+	local background = display.newImage("background.png")
+	--background.x = centerX
+	--background.y = centerY
+	
+	-- Background tranzparincy
+	background.alpha = .1
+	
+	-- Add planet to screen
+	local planet = display.newImage("planet.png")
+	planet.x = centerX
+	planet.y = display.contentHeight + 60
+	
+	-- Planet tranzparincy and size
+	planet.alpha = 0
+	planet.xScale = .1
+	planet.yScale = .1
+	
+	local function showTitle()
+		local gameTitle = display.newImage("gametitle.png")
+		gameTitle.alpha = 0
+		gameTitle.x = centerX 
+		gameTitle.y = centerY - 40
+		gameTitle.xScale = .3
+		gameTitle.yScale = .3
+		transition.to (gameTitle, {time=500, alpha=1, xScale=1, yScale=1, } )
+		spawnEnemy()
+	end
+	
+	transition.to (background, {time=2500, y=centerY, x=centerX, alpha=1} )
+	transition.to (planet, {time=2500, y=centerY, alpha=1, xScale=1, yScale=1, onComplete=showTitle} )
+	
+end	
+	
+
+
 
 createPlayScreen()
 startGame()
 
-spawnEnemy()
